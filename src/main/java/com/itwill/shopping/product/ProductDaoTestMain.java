8b01e62eab8961be88ea0d13eaaa961b4b6fb42b
dongjin403 <dongjin403@gmail.com>
2021-08-17 17:02:39 +0900

    ..

--- src/main/java/com/itwill/shopping/product/ProductDaoTestMain.java
+++ src/main/java/com/itwill/shopping/product/ProductDaoTestMain.java
@@ -4,7 +4,7 @@ public class ProductDaoTestMain {
 
 	public static void main(String[] args) throws Exception{
 		ProductDao productDao = new ProductDao();
-		System.out.println(productDao.getProduct(5)); 
+		System.out.println(productDao.getProduct(2)); 
 //		System.out.println(productDao.getProductListAsc()); 
 //		System.out.println(productDao.getProductListDesc()); 
 		
