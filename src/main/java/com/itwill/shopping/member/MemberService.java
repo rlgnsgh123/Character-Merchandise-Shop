b6d46b980442efe6301f4227969c12ae68412c4b
vlfwns924 <vlfwns924@gmail.com>
2021-08-17 19:35:44 +0900

    밑그림

--- src/main/java/com/itwill/shopping/member/MemberService.java
+++ src/main/java/com/itwill/shopping/member/MemberService.java
@@ -4,4 +4,32 @@ import com.itwill.shopping.member.MemberDao;
 
 public class MemberService {
 	private MemberDao memberDao;
+	
+	public MemberService() throws Exception {
+		memberDao = new MemberDao();
+	}
+	/*
+	 * 회원가입(아이디 중복 체크)
+	 */
+	
+	/*
+	 * 로그인(아이디 존재 여부, 패스워드 일치 여부)
+	 */
+	
+	/*
+	 * 회원전체리스트
+	 */
+	
+	/*
+	 * 회원 한명 보기
+	 */
+	
+	/*
+	 * 정보 수정
+	 */
+	
+	/*
+	 * 회원탈퇴
+	 */
+	
 }
