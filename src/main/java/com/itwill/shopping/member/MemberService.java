9bbda65d518fcb9337d69d02e231f5995696f4a1
vlfwns924 <vlfwns924@gmail.com>
2021-08-17 23:43:02 +0900

    오류메시지 제외 완성

--- src/main/java/com/itwill/shopping/member/MemberService.java
+++ src/main/java/com/itwill/shopping/member/MemberService.java
@@ -1,35 +1,64 @@
 package com.itwill.shopping.member;
 
+import java.util.ArrayList;
+
 import com.itwill.shopping.member.MemberDao;
 
 public class MemberService {
-	private MemberDao memberDao;
-	
+	private MemberDao memberDao;	
 	public MemberService() throws Exception {
 		memberDao = new MemberDao();
 	}
 	/*
 	 * 회원가입(아이디 중복 체크)
 	 */
+	public int create(Member member) throws Exception {
+		if(memberDao.idCheckCount(member.getM_id())==true) {
+			//이미 존재하는 아이디입니다.
+		}
+		return memberDao.create(member);	
+	}
 	
 	/*
 	 * 로그인(아이디 존재 여부, 패스워드 일치 여부)
 	 */
+	public Member login(String m_id,String m_password) throws Exception {
+		Member member = memberDao.selectById(m_id);
+		if(member==null) {
+			//존재하지 않는 아이디입니다.
+		}
+		if(!member.getM_password().equals(m_password)) {
+			//패스워드가 일치하지 않습니다.
+		}
+		return member;
+	}
 	
 	/*
 	 * 회원전체리스트
 	 */
+	public ArrayList<Member> findMemberList() throws Exception {
+		return memberDao.selectAll();
+	}
 	
 	/*
 	 * 회원 한명 보기
 	 */
+	public Member findMember(String m_id) throws Exception {
+		return memberDao.selectById(m_id);
+	}
 	
 	/*
 	 * 정보 수정
 	 */
+	public int updateMember(Member member) throws Exception {
+		return memberDao.update(member);
+	}
 	
 	/*
 	 * 회원탈퇴
 	 */
+	public int delete(String m_id) throws Exception {
+		return memberDao.deleteById(m_id);
+	}
 	
 }
