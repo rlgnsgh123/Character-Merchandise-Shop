4d766b88b6b79e494f9d422d98091d590145e65c
DESKTOP-9OIRTB9\전선우 <jsw1316@naver.com>
2021-08-17 15:31:58 +0900

    테스트

--- src/main/java/com/itwill/shopping/member/MemberDao.java
+++ src/main/java/com/itwill/shopping/member/MemberDao.java
@@ -1,5 +1,8 @@
 package com.itwill.shopping.member;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -21,4 +24,51 @@ public class MemberDao {
 		basicDataSource.setPassword(properties.getProperty("password"));
 		this.dataSource=basicDataSource;
 	}
+	
+	public int create(Member member) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		int insertRowCount = 0;
+		
+		try {
+			con = dataSource.getConnection();
+			pstmt = con.prepareStatement(MemberSQL.MEMBER_INSERT);
+			pstmt.setString(1, member.getM_id());
+			pstmt.setString(2, member.getM_password());
+			pstmt.setString(3, member.getM_name());
+			pstmt.setString(4, member.getM_address());
+			pstmt.setString(5, member.getM_phone());
+			insertRowCount = pstmt.executeUpdate();
+			return insertRowCount;					
+		} finally {
+			if(pstmt!=null) {
+				pstmt.close();
+			}
+			if(con!=null) {
+				con.close();
+			}
+		}				
+	}
+	
+	public int update(Member member) throws Exception {
+		return 0;
+		//테스트
+	}
+	
+	public Member selectById(String m_id) throws Exception {
+		return null;
+		
+	}
+	
+	public ArrayList<Member> selectAll() throws Exception {
+		return null;
+		
+	}
+	
+	
+	public int delete(String m_id) throws Exception {
+		return 0;
+	}
+	
+	
 }
