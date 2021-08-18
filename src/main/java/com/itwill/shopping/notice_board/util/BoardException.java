d7f8646e817a95ac54ccf54f2d815700bc519a9a
rlgnsgh123 <rlgnsgh123@gmail.com>
2021-08-18 15:49:23 +0900

    ..

--- src/main/java/com/itwill/shopping/notice_board/util/BoardException.java
+++ src/main/java/com/itwill/shopping/notice_board/util/BoardException.java
@@ -3,7 +3,7 @@
 // 회원에 관련된 사용자 정의 예외 클래스
 public class BoardException extends Exception {
 	public BoardException(){
-		this("게시판 관련 에러 발생");
+		this("고객센터 관련 에러 발생");
 	}
 	public BoardException(String errorMsg){
 		super(errorMsg);
