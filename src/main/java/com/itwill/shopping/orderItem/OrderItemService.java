9f8577e093781b48c80087af681c34fea610cbcb
sungyoonhee <momh2733@gmail.com>
2021-08-18 18:03:55 +0900

    완

--- src/main/java/com/itwill/shopping/orderItem/OrderItemService.java
+++ src/main/java/com/itwill/shopping/orderItem/OrderItemService.java
@@ -2,4 +2,13 @@ package com.itwill.shopping.orderItem;
 
 public class OrderItemService {
 
+	private OrderItemDao orderItemDao;
+	
+	private OrderItemService() throws Exception{
+		orderItemDao = new OrderItemDao();
+	}
+	//주문 한 개의  주문상세 여러 개 (주문상세)
+	public OrderItem oi_detail (int o_no) throws Exception{
+		return orderItemDao.oi_detail(o_no);
+	}
 }
